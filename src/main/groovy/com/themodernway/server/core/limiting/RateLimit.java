b5102580dab98b7459910c721c11fc0b7e51b894
deanjones <deanjones@deans-imac-pro>
2018-04-21 23:59:22 -0400

    2.2.0-RELEASE

--- src/main/groovy/com/themodernway/server/core/limiting/RateLimit.java
+++ src/main/groovy/com/themodernway/server/core/limiting/RateLimit.java
@@ -16,12 +16,14 @@
 
 package com.themodernway.server.core.limiting;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.util.concurrent.TimeUnit;
 
+@Documented
 @Target({ ElementType.TYPE, ElementType.METHOD, ElementType.ANNOTATION_TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RateLimit
--- src/main/groovy/com/themodernway/server/core/security/Authorized.java
+++ src/main/groovy/com/themodernway/server/core/security/Authorized.java
@@ -16,11 +16,13 @@
 
 package com.themodernway.server.core.security;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Documented
 @Target({ ElementType.TYPE, ElementType.METHOD, ElementType.ANNOTATION_TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Authorized
